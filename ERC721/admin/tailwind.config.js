69e19ddc7f03927365dac5d427d05a0723db4241
Sergey <mysteryon88@yandex.ru>
2024-07-12 11:09:14 +0300

    ERC721

---
+++ ERC721/admin/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  important: true,
+  darkMode: "class",
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
